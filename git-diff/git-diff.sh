e4ec5d7977aad9ee9779fad93262709243530c72
Caliari Jr <Calliari@users.noreply.github.com>
2021-12-10 09:19:55 +0000

    Update git-diff.sh

--- git-diff/git-diff.sh
+++ git-diff/git-diff.sh
@@ -1,5 +1,10 @@
-# Check what is changed before 'git commit'
+# Check what is changed before 'git commit' CMD
 git diff
 
-# After 'git add' & 'git commit' , and need to check what would be pushed run:
+# After 'git add' & 'git commit' CDM
+git diff --cached
+
+# After 'git add' & 'git commit' , comparing branches
 git diff HEAD^ HEAD
+git diff ..master    # compare current branch with 'master'
+git diff dev..master # compare 'dev' with 'master'

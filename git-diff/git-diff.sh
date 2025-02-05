0035d92da8b7cca281ce107766319c0780bede03
Osvaldo Caliari <Calliari@users.noreply.github.com>
2025-02-05 16:33:25 +0000

    Update git-diff.sh

--- git-diff/git-diff.sh
+++ git-diff/git-diff.sh
@@ -6,5 +6,6 @@ git diff --cached
 
 # After 'git add' & 'git commit' , comparing branches
 git diff HEAD^ HEAD
-git diff ..master    # compare current branch with 'master'
-git diff dev..master # compare 'dev' with 'master'
+git diff ..master     # compare current branch with 'master'
+git diff dev..master  # compare 'dev' with 'master'
+git diff master..     # compare master with current branch (what we have in master is shown as '-' minus)

f4ddf70721f6bb2c8233175f338bdb86f870fb10
Osvaldo Caliari <Calliari@users.noreply.github.com>
2025-02-07 12:11:58 +0000

    Update git-diff.sh

--- git-diff/git-diff.sh
+++ git-diff/git-diff.sh
@@ -17,10 +17,10 @@ git checkout master
 git checkout master -- .
 git pull origin master   
 
-# here is the test
+# here is the test (merging the fearure branch inot the master branch locally without any interaction with the remote)
 git merge --no-commit --no-ff feature/branch-123456
 
-# Here are the files which will be changing with the merge on the remote (but for now its only on the local-machine)
+# Here are the files which will be changing with the merge on the remote master branch (but for now its only on the local-machine)
 git status 
 
 # Here we can see the lines that will be changed on this file in particular and must match the created pull-request difference that will be merged into the master branch.
